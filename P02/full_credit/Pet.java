453b188fce6d3a92afbece00a58b90bba8167dd1
hrt8500 <hrt8500@mavs.uta.edu>
2023-01-27 22:03:29 -0600

    P02 Full Credit

---
+++ P02/full_credit/Pet.java
@@ -0,0 +1,26 @@
+public class Pet {
+    private String name;
+    private Type type;
+    private double age;
+
+    public Pet(String name, Type type, double age) {
+        this.name = name;
+        this.type = type;
+        this.age = age;
+    }
+    
+    @Override
+    public String toString() {
+        return name + " is a " + type + " age " + age;
+    }
+
+    Pet[] pets = {new Pet("Lola", Pet.Type.Horse, 6),
+    new Pet("Shadow", Pet.Type.Fish, 2),
+    new Pet("Chloe", Pet.Type.Turtle, 52),
+    new Pet("Oreo", Pet.Type.Wolf, 4)};
+
+    for (Pet pet : pets) {
+        System.out.println(pet);
+    }
+}
+
---
+++ P02/full_credit/Type.java
@@ -0,0 +1,3 @@
+public enum Type {
+    Horse, Fish, Turtle, Wolf
+}
